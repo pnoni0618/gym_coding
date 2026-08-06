e0dc767908db1941ca1348c090d7fe00d4a98bb2
pnoni0618 <pnoni0618@gmail.com>
2026-08-06 14:54:26 +0900

    user memory 설정실습

---
+++ user-memory-setup/src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <section id="center">
+      <h1>카운터 앱</h1>
+      <Counter />
+    </section>
+  );
+}
+
+export default App;
